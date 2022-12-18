e2becb9374659518eb32d1d80c77fbe6a1c9ede9
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-18 19:27:01 +0530

    autoupdate

--- bash/sleep.sh
+++ bash/sleep.sh
@@ -8,5 +8,5 @@ echo "wait for 9 seconds"
 sleep 9s
 # you can also use "sleep 9" in place of "sleep 9s" because if there is no suffix, it is considered as "seconds"
 #end time
-date+"%H:%M:%S"
+date +"%H:%M:%S"
 echo "Task Completed"

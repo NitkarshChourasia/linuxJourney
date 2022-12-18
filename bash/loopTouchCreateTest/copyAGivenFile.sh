24e56798d6772e4cc1e4660d4ef670d93f2c1e9d
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-19 04:36:01 +0530

    autoupdate 2022-12-19-04:36:01

--- bash/loopTouchCreateTest/copyAGivenFile.sh
+++ bash/loopTouchCreateTest/copyAGivenFile.sh
@@ -1,4 +1,7 @@
 for i in {0..9999}
 do
-  cp /home/nitkarshc/Desktop/Programmed?/linuxJourney/bash/loopTouchCreateTest/touchLoop.sh
+  cp /home/nitkarshc/Desktop/Programmed?/linuxJourney/bash/loopTouchCreateTest/touchLoop.sh /home/nitkarshc/Desktop/Programmed?/linuxJourney/bash/loopTouchCreateTest/loopCopyTest
 done
+
+# So I want to create a kind of bash which copies file to a certain destination with a certain incremented variable name in it.
+# That is what I want to create.
